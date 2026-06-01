31c16c76a7115544a78d2d1e0b14a5a8edd52204
RiaDhanani <ria08.dhanani@gmail.com>
2026-06-01 13:00:15 -0500

    Refine subscribe block and footer layout

--- app/components/BeehiivEmbed.js
+++ app/components/BeehiivEmbed.js
@@ -1,12 +1,16 @@
 'use client'
-import Script from 'next/script'
+import { useEffect, useRef } from 'react'
 
 export default function BeehiivEmbed() {
-  return (
-    <Script
-      src="https://subscribe-forms.beehiiv.com/v3/loader.js"
-      data-beehiiv-form="30753988-de1d-4052-a32e-81ad0090a77d"
-      strategy="afterInteractive"
-    />
-  )
+  const ref = useRef(null)
+
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://subscribe-forms.beehiiv.com/v3/loader.js'
+    script.async = true
+    script.setAttribute('data-beehiiv-form', '30753988-de1d-4052-a32e-81ad0090a77d')
+    ref.current?.appendChild(script)
+  }, [])
+
+  return <div ref={ref} />
 }
